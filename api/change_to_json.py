2a2c74a2edaa4ffabf68f30ac285924c52025471
amin <aminelfaquiri0@gmail.com>
2023-12-05 11:43:40 +0100

    feat : change the text t data frame

--- api/change_to_json.py
+++ api/change_to_json.py
@@ -2,13 +2,27 @@
 import pandas as pd
 import json
 
+
+# change txt file to dataframe :
 def change_to_df(filpath,splitter,columns) :
 
-    df = pd.read_csv(filpath, sep=splitter, header=None, names=columns)
+    df = pd.read_csv(filpath, sep=splitter, header=None, names=columns,encoding='latin-1')
     return df
 
-user_rating = ['userId', 'movieId', 'rating', 'timestamp']
+# define the column name :
 movies_review = ["movieId","movie_title","release_date","error","IMDb_URL","unknown","Action","Adventure","Animation","Children's","Comedy","Crime","Documentary","Drama","Fantasy","Film-Noir","Horror","Musical","Mystery","Romance","Sci-Fi","Thriller","War","Western"]
+user_rating = ['userId', 'movieId', 'rating', 'timestamp']
+user_info = ["userId","age","gender","occupation","zip"]
 
-user_df = change_to_df('api/data/data.txt','\t',user_rating)
-# item_df = change_to_df('api/data/item.txt','|',movies_review)
+
+# Change all file to dataframe :
+movies_df = change_to_df('api/data/item.txt','|',movies_review).drop('error', axis=1)
+rating_df = change_to_df('api/data/data.txt','\t',user_rating)
+user_df = change_to_df('api/data/user.txt','|',user_info)
+
+# merge between user and rating dataframe :
+rating_df = pd.merge(rating_df, user_df, on="userId").drop(['occupation','zip'], axis=1)
+
+
+print(rating_df)
+
