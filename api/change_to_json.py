243ecc137817647cddc62b76ddc526300c02b5d0
amin <aminelfaquiri0@gmail.com>
2023-12-05 11:17:26 +0100

    modifie : fixe the proble of directory and back the file in api file

--- api/change_to_json.py
+++ api/change_to_json.py
@@ -1,1 +1,14 @@
 
+import pandas as pd
+import json
+
+def change_to_df(filpath,splitter,columns) :
+
+    df = pd.read_csv(filpath, sep=splitter, header=None, names=columns)
+    return df
+
+user_rating = ['userId', 'movieId', 'rating', 'timestamp']
+movies_review = ["movieId","movie_title","release_date","error","IMDb_URL","unknown","Action","Adventure","Animation","Children's","Comedy","Crime","Documentary","Drama","Fantasy","Film-Noir","Horror","Musical","Mystery","Romance","Sci-Fi","Thriller","War","Western"]
+
+user_df = change_to_df('api/data/data.txt','\t',user_rating)
+# item_df = change_to_df('api/data/item.txt','|',movies_review)
