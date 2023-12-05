2f1a355cc2f4155a72d7530f0f61ac2ab2f1011f
amin <aminelfaquiri0@gmail.com>
2023-12-05 11:57:54 +0100

    feat : save the data as json

--- api/change_to_json.py
+++ api/change_to_json.py
@@ -23,6 +23,42 @@ user_df = change_to_df('api/data/user.txt','|',user_info)
 # merge between user and rating dataframe :
 rating_df = pd.merge(rating_df, user_df, on="userId").drop(['occupation','zip'], axis=1)
 
+i = 0
 
-print(rating_df)
+# change the dataframe to json file :
+result = []
+for _, entry in rating_df.iterrows():
+    user_id = entry['userId']
+    movie_id = entry['movieId']
+    rating = entry['rating']
+    timestamp = entry['timestamp']
+    age = entry['age']
+    gender = entry['gender']
 
+    # Check if movie_id exists in item_df :
+    movie_data = movies_df[movies_df['movieId'] == movie_id]
+    if not movie_data.empty:
+        movie_data = movie_data.iloc[0]
+        title = movie_data['movie_title']
+        genres = [genre for genre, value in movie_data.items() if value == 1 and genre != 'movieId']
+
+        fishies_entry = {
+            'userId': user_id,
+            'age': age,
+            'gender' :gender,
+            'movie': {
+                'movieId': movie_id,
+                'title': title,
+                'genres': genres
+            },
+            'rating': rating,
+            'timestamp': timestamp
+        }
+
+        result.append(fishies_entry)
+        print(i)
+        i += 1
+
+# Save this to a JSON file :
+with open('movies_rating.json', 'w') as f:
+    json.dump(result, f)
